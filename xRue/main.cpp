a092bcb04ec2c94220e887a4df223b923ae22de1
Adam Willats <adam@lawn-128-61-3-195.lawn.gatech.edu>
2017-07-19 11:45:25 -0400

    Initial Commit

---
+++ xRue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xRue
+//
+//  Created by Adam Willats on 7/19/17.
+//  Copyright © 2017 Adam Willats. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
